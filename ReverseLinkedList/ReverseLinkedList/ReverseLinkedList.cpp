fcc16e09aeb9efc3407cae5333b4a44cdb6af971
Harsh Chheda <harshchheda86@gmail.com>
2024-09-29 17:41:50 -0700

    Reverse Linked List

---
+++ ReverseLinkedList/ReverseLinkedList/ReverseLinkedList.cpp
@@ -0,0 +1,47 @@
+// ReverseLinkedList.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+
+
+// Definition for singly-linked list.
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+
+ListNode* reverseList(ListNode* head) {
+    if (!head || !head->next) return head;
+
+    ListNode* prev = nullptr;
+
+    while (head != nullptr)
+    {
+        ListNode* next = head->next;
+        head->next = prev;
+        prev = head;
+        head = next;
+    }
+
+    return prev;
+}
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
