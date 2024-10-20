c39f91f6f985b1b6986fba2c1baf3f775936a291
Harsh Chheda <harshchheda86@gmail.com>
2024-10-20 16:15:52 -0700

    is number a perfect square

---
+++ IsPerfectSquare/IsPerfectSquare/IsPerfectSquare.cpp
@@ -0,0 +1,49 @@
+// IsPerfectSquare.cpp : This file contains the 'main' function. Program execution begins and ends there.
+/*
+Given a positive integer num, return true if num is a perfect square or false otherwise.
+
+A perfect square is an integer that is the square of an integer. In other words, it is the product of some integer with itself.
+
+You must not use any built-in library function, such as sqrt.
+
+Example 1:
+
+Input: num = 16
+Output: true
+Explanation: We return true because 4 * 4 = 16 and 4 is an integer.
+Example 2:
+
+Input: num = 14
+Output: false
+Explanation: We return false because 3.742 * 3.742 = 14 and 3.742 is not an integer.
+*/
+
+#include <iostream>
+
+bool isPerfectSquare(int num) {
+    long int square = 1, i = 0;
+
+    while (square < num)
+    {
+        square = i * i;
+        i++;
+    }
+
+    return (square == num);
+}
+
+int main()
+{
+    std::cout << isPerfectSquare(16);
+}
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
